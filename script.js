41d2165aaf1d2971292d720da85f0d7908446f83
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-10 20:34:09 +0200

    add calulator object

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 
-
+const Calculator = {
+    num1 : 0,
+    num2 : 0,
+    symbol : '+',
+    operate() {
+        switch (this.symbol) {
+            case '+':
+                return add(this.num1, this.num2);
+                break;
+            case '-':
+                return subtract(this.num1, this.num2);
+                break;
+            case '*':
+                return multiply(this.num1, this.num2);
+                break;
+            case '/':
+                return divide(this.num1, this.num2);
+                break;
+            default:
+                return "Error: Invalid operation";
+        }
+    }
+};
 
 function add (num1, num2) {
     return num1 + num2
