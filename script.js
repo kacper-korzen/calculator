f71d264da1a24c5c6d2d04b37136a57756b403b5
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-12 00:11:05 +0200

    add functions to calculate results from input

--- script.js
+++ script.js
@@ -2,81 +2,118 @@
 
 let num1 = 0;
 let num2 = 0;
-let symbol = '';
+let operation = ' ';
 let operatorClicked = false;
+let resultClicked = false;
+let dotClicked = false;
 
-const Calculator = {
-    num1 : 0,
-    num2 : 0,
-    symbol : '',
-    operate() {
-        switch (this.symbol) {
-            case '+':
-                return add(this.num1, this.num2);
-                break;
-            case '-':
-                return subtract(this.num1, this.num2);
-                break;
-            case 'x':
-                return multiply(this.num1, this.num2);
-                break;
-            case '/':
-                return divide(this.num1, this.num2);
-                break;
-            default:
-                return "Error: Invalid operation";
-        }
+function Calculator(num1, num2, symbol) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.symbol = symbol;
+    this.result = 0;
+}
+
+Calculator.prototype.operate = function() {
+    switch (this.symbol) {
+        case '+':
+            return add(this.num1, this.num2);
+        case '-':
+            return subtract(this.num1, this.num2);
+        case 'x':
+            return multiply(this.num1, this.num2);
+        case '/':
+            return divide(this.num1, this.num2);
+        default:
+            return "Error: Invalid operation";
     }
 };
 
 function add (num1, num2) {
-    return num1 + num2
+    return (num1 + num2).toFixed(2);
 }
 
 function subtract (num1, num2) {
-    return num1 - num2
+    return (num1 - num2).toFixed(2);
 }
 
 function multiply (num1, num2) {
-    return num1 * num2;
+    return (num1 * num2).toFixed(2);
 }
 
 function divide (num1, num2) {
     if (num2 === 0) {
-        return 'Error: Cannot divide by zero'
+        return 'Zero division error';
     }
-    return num1 / num2;
+    return (num1 / num2).toFixed(2);
 }
 
 // TODO: add validation
-function setOperationComponents(str){
-    const components = str.split(' ').sort();
+function calculate(str){
+    const components = str.trim().split(operation);
 
-    if (components.length < 3 || components.includes('')) {
-        console.log("za mało", components);
+    if (!components.length < 2 && !components.includes('') ||components.includes(' ')) {
+        [num1, num2] = components;
+        const calculate = new Calculator(parseFloat(num1), parseFloat(num2), operation);
+        result.innerHTML = calculate.operate();        
     } else {
-        console.log('dziala', components);
+        input.innerHTML = str;
+        enableButtons();
     }
 }
 
+function disableButtons() {
+    calculatorButtons.map(button => {
+        if (button.textContent !== 'AC') {
+            button.setAttribute('disabled', true);
+        }
+    });
+}
+
+function enableButtons() {
+    calculatorButtons.map(button => {
+        if (button.textContent !== 'AC') {
+            button.removeAttribute('disabled');
+        }
+    });
+}
+
+
+const input = document.querySelector('.input');
+const result = document.querySelector('.result');
 function showInput(symbol) {
-    const input = document.querySelector('.input');
-    
-    let inputValue = '';
     const operations = '+-/x';
-    
+
     if (!isNaN(symbol)) {
         input.textContent += symbol;
     } else if (operations.includes(symbol) && !operatorClicked && input.innerHTML !== '&nbsp;') {
-        input.textContent += ` ${symbol} `;
+        input.textContent += `${symbol}`;
         operatorClicked = true; 
+        operation = symbol;
+        if (input.textContent.split(operation).length > 1) {
+            dotClicked = false;
+        }
+    } else if(symbol === '=' && operatorClicked === true && input.textContent) {
+        resultClicked = true;
+        disableButtons();
+        calculate(input.textContent);
 
-    } else if(symbol === '=') {
-        // setOperationComponents(input.textContent);
+    } else if(symbol === 'AC') {
         input.innerHTML = '&nbsp;';
+        result.innerHTML = '0';
+        enableButtons();
         operatorClicked = false;
+        dotClicked = false;
+    } else if(symbol === '←' && !input.textContent.includes(operations) && input.innerHTML !== '&nbsp;' ) {
+        input.textContent = input.textContent.slice(0, -1);
+        operatorClicked = false;
+    } else if(symbol === '√') {
+        disableButtons();
+        result.innerHTML = Math.sqrt(input.textContent);
+    } else if (symbol === '.' && !dotClicked) {
+        dotClicked = true;
+        input.textContent += symbol;
     }
-
     
 }
 
@@ -85,5 +122,7 @@ const calculatorButtons = Array.from(calculatorButtonsDiv.querySelectorAll('butt
 
 
 calculatorButtons.map(button => {
-    button.addEventListener('click', () => { showInput(button.textContent)});
+    button.addEventListener('click', () => { 
+        showInput(button.textContent)
+    });
 });
