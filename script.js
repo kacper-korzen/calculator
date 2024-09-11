2234752d5aab8fba6625fdf3dba817f3c7e66cdd
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-11 14:01:09 +0200

    add function to show numers and symbols on display

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 let num1 = 0;
 let num2 = 0;
 let symbol = '';
+let operatorClicked = false;
 
 const Calculator = {
     num1 : 0,
     num2 : 0,
-    symbol : '+',
+    symbol : '',
     operate() {
         switch (this.symbol) {
             case '+':
@@ -47,11 +48,42 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
+// TODO: add validation
+function setOperationComponents(str){
+    const components = str.split(' ').sort();
+
+    if (components.length < 3 || components.includes('')) {
+        console.log("za mało", components);
+    } else {
+        console.log('dziala', components);
+    }
+}
+
+function showInput(symbol) {
+    const input = document.querySelector('.input');
+    
+    let inputValue = '';
+    const operations = '+-/x';
+    
+    if (!isNaN(symbol)) {
+        input.textContent += symbol;
+    } else if (operations.includes(symbol) && !operatorClicked && input.innerHTML !== '&nbsp;') {
+        input.textContent += ` ${symbol} `;
+        operatorClicked = true; 
+
+    } else if(symbol === '=') {
+        // setOperationComponents(input.textContent);
+        input.innerHTML = '&nbsp;';
+        operatorClicked = false;
+    }
+
+    
+}
 
 const calculatorButtonsDiv = document.querySelector('.calculator-buttons');
 const calculatorButtons = Array.from(calculatorButtonsDiv.querySelectorAll('button'));
 
 
-calculatorButtons.map(button =>{
+calculatorButtons.map(button => {
     button.addEventListener('click', () => { showInput(button.textContent)});
 });
